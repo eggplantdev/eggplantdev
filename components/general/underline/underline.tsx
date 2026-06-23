972b15b24aa1cb9064faf93dbf44ebb8e9996c5b
ex-Plant <konradantonik@gmail.com>
2026-06-23 20:43:08 +0200

    refactor(styles): use left-1/2 instead of arbitrary left-[50%]
    v4 emits the centering utility directly; no need for the bracket value.

--- components/general/underline/underline.tsx
+++ components/general/underline/underline.tsx
@@ -2,7 +2,7 @@ export const Underline = () => {
   return (
     <span
       className={
-        "absolute bottom-[-3px] left-[50%] h-[3px] w-0 rounded bg-white duration-100" +
+        "absolute bottom-[-3px] left-1/2 h-[3px] w-0 rounded bg-white duration-100" +
         " group-hover:left-0 group-hover:w-full"
       }
     ></span>
